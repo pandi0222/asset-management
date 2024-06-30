faeefcc2d1042d93bcc48adb9b373c336f2c7d65
Pandeeswaran B <pandi220520000@gmail.com>
2024-06-30 20:30:28 +0530

    updated

---
+++ routes/assetCategory.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+// Add your routes here for Asset Category Master
+
+module.exports = router;
---
+++ routes/employee.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+// Add your routes here for Employee Master
+
+module.exports = router;
